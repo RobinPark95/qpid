e1ba3884807f10ca4e505d77e12296489c1f60fc
linhao.piao <linhao.piao@sumscope.com>
2020-07-31 11:13:01 +0800

    first push

---
+++ src/main/java/org/apache/qpid/jms/example/PriorityServer.java
@@ -0,0 +1,54 @@
+package org.apache.qpid.jms.example;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class PriorityServer {
+    public static void main(String[] args) throws Exception {
+        try {
+            // The configuration for the Qpid InitialContextFactory has been supplied in
+            // a jndi.properties file in the classpath, which results in it being picked
+            // up automatically by the InitialContext constructor.
+            Context context = new InitialContext();
+
+            ConnectionFactory factory = (ConnectionFactory) context.lookup("myFactoryLookup");
+
+            Queue ConsumerQueue = (Queue) context.lookup("myPriorityLookup");
+
+            Connection connection = factory.createConnection("guest", "guest");
+            connection.setExceptionListener(new PriorityServer.MyExceptionListener());
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            MessageConsumer messageConsumer = session.createConsumer(ConsumerQueue);
+            MessageProducer messageProducer = session.createProducer(null);
+
+            while (true) {
+                //Receive messages and return a new uppercase message.
+                TextMessage requestMessage = (TextMessage) messageConsumer.receive();
+
+                System.out.println("[SERVER] Received: " + requestMessage.getText());
+
+                TextMessage responseMessage = session.createTextMessage(requestMessage.getText().toUpperCase());
+
+                messageProducer.send(requestMessage.getJMSReplyTo(), responseMessage, DeliveryMode.NON_PERSISTENT, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
+            }
+
+        } catch (Exception exp) {
+            System.out.println("[SERVER] Caught exception, exiting.");
+            exp.printStackTrace(System.out);
+            System.exit(1);
+        }
+    }
+
+    private static class MyExceptionListener implements ExceptionListener {
+        @Override
+        public void onException(JMSException exception) {
+            System.out.println("[SERVER] Connection ExceptionListener fired, exiting.");
+            exception.printStackTrace(System.out);
+            System.exit(1);
+        }
+    }
+}
